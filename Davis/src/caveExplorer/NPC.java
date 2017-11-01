3d34551572922680f83a075ae7950ff3bc0abf04
BT_1N3_13 <BT_1N3_13@BT_1N3_13.bktech.local>
2017-11-01 15:33:31 -0400

    asdf

--- Davis/src/caveExplorer/NPC.java
+++ Davis/src/caveExplorer/NPC.java
@@ -1,33 +1,75 @@
 package caveExplorer;
 
 public class NPC {
-
+	private CaveRoom[][] floor;
+	private int currentRow;
+	private int currentCol;
+	private NPCRoom currentRoom;
+	private boolean active;
+	private String activeDescription;
+	private String inactiveDescription;
 	public NPC() {
-		// TODO Auto-generated constructor stub
+		this.floor = CaveExplorer.caves;
+		this.activeDescription = "There is a person standing in the room, waiting to talk to you. Press f to talk.";
+		this.inactiveDescription = "The person you spoke to earlier is standing here.";
+		this.currentCol = -1;
+		this.currentRow = -1;
+		currentRoom = null;
+		active = true;
 	}
 
 	public boolean isActive() {
-		// TODO Auto-generated method stub
-		return false;
+		return active;
 	}
 
 	public void interact() {
-		// TODO Auto-generated method stub
-		
+		CaveExplorer.print("lets talk type bye to stop");
+		String s = CaveExplorer.in.nextLine();
+		while (!s.equalsIgnoreCase("bye")) {
+			CaveExplorer.print("ok dad");
+			s = CaveExplorer.in.nextLine();
+		}
+		CaveExplorer.print("Lata nerd");
+		active = false;
 	}
 
 	public String getDescription() {
-		// TODO Auto-generated method stub
-		return null;
+		return activeDescription;
 	}
 
 	public String getInactiveDescription() {
-		// TODO Auto-generated method stub
-		return null;
+		return inactiveDescription;
 	}
 
 	public String getSymbol() {
-		// TODO Auto-generated method stub
-		return null;
+		return "P";
 	}
+
+	public void setPosition(int i, int j) {
+		if(i>=0 && i<floor.length && j>=0 && j<floor[i].length &&floor[i][j] instanceof NPCRoom) {
+			if(currentRoom != null) {
+				currentRoom.leaveNPC();
+			}
+			currentRow = i;
+			currentCol= j;
+			currentRoom = (NPCRoom)floor[i][j];
+			currentRoom.enterNPC(this);
+		}
+	}
+
+	public void autoMove() {
+		if(active) {
+			int[] move = calculateMove();
+			int newRow = currentRow + move[0];
+			int newCol = currentCol + move[1];
+			setPosition(newRow,newCol);
+		}
+		
+	}
+
+	private int[] calculateMove() {
+		int[][] posibleMoves = {{-1,0},{0,1},{1,0},{0,-1}};
+		int index =
+	}	
 }
+
